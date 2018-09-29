4459166e606523fa87927707f5ed341510661716
Mattias Eriksson <thematerik@gmail.com>
2018-09-29 17:52:42 +0200

    [plugin] fetch harmonyhubs

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 
-var Homebridge = require('./lib/homebridge');
-var Service, Characteristic, Accessory, uuid;
+var Plugin = require('./lib/plugin');
 
 module.exports = function(homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
-	Accessory = homebridge.hap.Accessory;
-	uuid = homebridge.hap.uuid;
-
-	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", new Homebridge(), true);
+	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", new Plugin(homebridge), true);
 };

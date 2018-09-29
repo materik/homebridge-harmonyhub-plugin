c24eb30d4396a7b1507991cae2cb212637ac6808
Mattias Eriksson <thematerik@gmail.com>
2018-09-29 18:04:19 +0200

    [lib] handle log

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 var Plugin = require('./lib/plugin');
 
 module.exports = function(homebridge) {
-	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", new Plugin(homebridge), true);
+    // TODO
+    var Service = homebridge.hap.Service;
+    var Characteristic = homebridge.hap.Characteristic;
+    var Accessory = homebridge.hap.Accessory;
+    var uuid = homebridge.hap.uuid;
+
+	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", Plugin, true);
 };

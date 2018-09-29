a358160d8efc8661470ce95f4a46fd26c7f696a3
Mattias Eriksson <thematerik@gmail.com>
2018-09-29 17:37:26 +0200

    [lib] start from scratch

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 var Homebridge = require('./lib/homebridge');
-
 var Service, Characteristic, Accessory, uuid;
 
 module.exports = function(homebridge) {
@@ -9,16 +8,5 @@ module.exports = function(homebridge) {
 	Accessory = homebridge.hap.Accessory;
 	uuid = homebridge.hap.uuid;
 
-	var exportedTypes = {
-		Service: homebridge.hap.Service,
-		Characteristic: homebridge.hap.Characteristic,
-		Accessory: homebridge.hap.Accessory,
-		PlatformAccessory: homebridge.platformAccessory,
-		uuid: homebridge.hap.uuid
-	};
-	
-	exportedTypes.ActivityAccessory = require('./lib/activity-accessory')(exportedTypes);
-	exportedTypes.PlatformAccessory = require('./lib/platform-accessory')(exportedTypes);
-
-	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", Homebridge(exportedTypes), true);
+	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", new Homebridge(), true);
 };

8f43550cf0f2d7f8def446e6b0b47605e81c8c5b
Mattias Eriksson <thematerik@gmail.com>
2018-09-29 19:36:44 +0200

    [lib] fix issues

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 var Plugin = require('./lib/plugin');
 
 module.exports = function(homebridge) {
-    global.Service = homebridge.hap.Service;
-    global.Characteristic = homebridge.hap.Characteristic;
-    global.Accessory = homebridge.hap.Accessory;
-    global.uuid = homebridge.hap.uuid;
+    global.HomebridgeService = homebridge.hap.Service;
+    global.HomebridgeCharacteristic = homebridge.hap.Characteristic;
+    global.HomebridgeAccessory = homebridge.hap.Accessory;
+    global.HomebridgeIdentifier = homebridge.hap.uuid;
 
 	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", Plugin, true);
 };

671b9d715a03ceabc907627fa7c3fe86f495e039
Mattias Eriksson <thematerik@gmail.com>
2018-09-30 11:35:53 +0200

    [project] initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let Plugin = require('./lib/plugin');
+
+module.exports = function(homebridge) {
+    global.HomebridgeAccessory = homebridge.hap.Accessory;
+    global.HomebridgeCharacteristic = homebridge.hap.Characteristic;
+    global.HomebridgeIdentifier = homebridge.hap.uuid;
+    global.HomebridgePlatformAccessory = homebridge.platformAccessory;
+    global.HomebridgeService = homebridge.hap.Service;
+
+	homebridge.registerPlatform("homebridge-harmonyhub", "HarmonyHub", Plugin, true);
+};
